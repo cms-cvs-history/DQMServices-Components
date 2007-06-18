08a618248463e0567e65b20899a383a6a9c83069
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2007-06-18 13:29:04 +0000

    recommitted tag version V00-03-67 to end of branch_1_4_X

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -1,5 +1,4 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-
+#include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/InputSourceMacros.h"
 
 #include "DQMServices/Components/interface/DQMEventSource.h"
@@ -11,6 +10,3 @@ DEFINE_FWK_MODULE(EventCoordinatesSource);
 // The DQM Client input source
 DEFINE_ANOTHER_FWK_INPUT_SOURCE(DQMEventSource);
 
-// The help class running the quality tests
-#include "DQMServices/Components/src/QualityTester.h"
-DEFINE_ANOTHER_FWK_MODULE(QualityTester);
