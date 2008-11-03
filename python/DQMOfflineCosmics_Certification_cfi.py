39b5b910ab19f11cc92d0c7e36799101afdb7e3a
cmsbuild <>
2008-11-03 15:44:52 +0000

    This commit was manufactured by cvs2git to create tag 'DQMServices- Components-V03-03-11'.
    Sprout from master 2008-11-03 15:27:34 UTC Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch> ''
    Cherrypick from master 2008-08-27 17:06:33 UTC Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch> 'Add more flexibility on how references are handled on saving.':
        BuildFile
        src/DQMFileSaver.h
    Cherrypick from master 2008-11-03 15:44:51 UTC Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch> '':
        src/DQMFEDIntegrityClient.cc
    Delete:
        data/DQMOfflineCosmics_Certification.cff
        data/DQMOfflineCosmics_DAQCertification.cfi
        python/DQMOfflineCosmics_Certification_cff.py
        python/DQMOfflineCosmics_Certification_cfi.py
        src/DQMDaqInfo.cc
        src/DQMDaqInfo.h
        test/dqmDaqDataCertification.cfg
        test/dqmDaqDataCertification_cfg.py
        test/test_good_offline_run.cfg
        test/test_good_offline_run_withref_all.cfg
        test/test_good_offline_run_withref_qtests.cfg
        test/test_good_offline_run_withref_skip.cfg
        test/test_good_online_run_withref_all.cfg
        test/test_good_online_run_withref_qtests.cfg
        test/test_good_online_run_withref_skip.cfg

--- python/DQMOfflineCosmics_Certification_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-DaqData = cms.EDFilter("DQMDaqInfo",
-    saveDCFile = cms.untracked.bool(True),
-    outputFile = cms.string('dqmDaqInfo.txt'),
-    saveRootFile = cms.bool(True),
-    OutputFileName = cms.string('DQMDAQDataCert.root')
-)
