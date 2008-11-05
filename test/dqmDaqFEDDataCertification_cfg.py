7b73f624ed85453a86468baf63f935b40ceae35f
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2008-11-05 13:59:19 +0000

    calculates feds percentages per subdetector

--- test/dqmDaqFEDDataCertification_cfg.py
+++ test/dqmDaqFEDDataCertification_cfg.py
@@ -4,7 +4,7 @@ process = cms.Process("DataCert")
 process.load("CondCore.DBCommon.CondDBCommon_cfi")
 process.load("DQMServices.Components.DQMOfflineCosmics_Certification_cff")
 
-process.DaqData.FedGranularityInput = False
+process.DaqData.FedGranularityInput = True
 process.DaqData.saveDCFile = False
 
 process.source = cms.Source("PoolSource",
@@ -20,7 +20,7 @@ process.DQMStore = cms.Service("DQMStore",
 
 
 
-process.PoolSource.fileNames = ['/store/data/Commissioning08/Cosmics/RECO/v1/000/064/257/04264E22-CD90-DD11-80EF-000423D9939C.root']
+
 process.CondDBCommon.connect = 'oracle://cms_orcoff_prod/CMS_COND_21X_RUN_INFO'
 process.CondDBCommon.DBParameters.authenticationPath = '/afs/cern.ch/cms/DB/conddb/'
 
@@ -28,10 +28,15 @@ process.rn = cms.ESSource("PoolDBESSource",
     process.CondDBCommon,
     timetype = cms.string('runnumber'),
     toGet = cms.VPSet(cms.PSet(
-	record = cms.string('RunSummaryRcd'),
-	tag = cms.string('runsummary_test')
+	record = cms.string('RunInfoRcd'),
+	tag = cms.string('runinfo_test')
     ))
 )
+
+
+process.PoolSource.fileNames = ['/store/data/Commissioning08/Cosmics/RECO/v1/000/066/394/229A4AEE-359B-DD11-B144-000423D99B3E.root']
+
+
 
 process.asciiprint = cms.OutputModule("AsciiOutputModule")
 
