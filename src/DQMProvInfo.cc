b9dc58ff9618092fc0afddad1f80fe8b26df463d
cmsbuild <>
2009-12-11 13:11:17 +0000

    This commit was manufactured by cvs2git to create tag 'DQMServices- Components-V03-09-10'.
    Sprout from master 2009-11-29 17:28:46 UTC Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch> '*** empty log message ***'
    Cherrypick from master 2009-10-28 10:12:11 UTC Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch> '':
        plugins/EDMtoMEConverter.cc
        plugins/EDMtoMEConverter.h
        plugins/MEtoEDMConverter.cc
        plugins/MEtoEDMConverter.h
        python/EDMtoMEConverter_cfi.py
    Cherrypick from master 2009-12-11 13:11:16 UTC Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch> '*** empty log message ***':
        BuildFile
        src/DQMFEDIntegrityClient.cc
        src/DQMProvInfo.cc
        src/DQMProvInfo.h

--- src/DQMProvInfo.cc
+++ src/DQMProvInfo.cc
@@ -2,14 +2,18 @@
  * \file DQMEventInfo.cc
  * \author M. Zanetti - CERN PH
  * Last Update:
- * $Date: 2009/09/06 11:10:15 $
- * $Revision: 1.24 $
+ * $Date: 2009/12/11 01:08:34 $
+ * $Revision: 1.3 $
  * $Author: ameyer $
  *
  */
 
 #include "DQMProvInfo.h"
 #include <TSystem.h>
+#include "DataFormats/Scalers/interface/DcsStatus.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/L1GlobalTrigger/interface/L1GtFdlWord.h"
+#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerReadoutRecord.h"
 
 // Framework
 
@@ -27,25 +31,121 @@ DQMProvInfo::DQMProvInfo(const ParameterSet& ps){
 
   dbe_ = edm::Service<DQMStore>().operator->();
 
-  string provinfofolder = parameters_.getUntrackedParameter<string>("provInfoFolder", "ProvInfo") ;
-  string subsystemname = parameters_.getUntrackedParameter<string>("subSystemFolder", "YourSubsystem") ;
-  currentfolder_ = subsystemname + "/" +  provinfofolder ;
-
+  provinfofolder_ = parameters_.getUntrackedParameter<string>("provInfoFolder", "ProvInfo") ;
+  subsystemname_ = parameters_.getUntrackedParameter<string>("subSystemFolder", "YourSubsystem") ;
+  makedcsinfo_ = parameters_.getUntrackedParameter<bool>("makeDcsInfo",true);
+  dcsword_=0xffff; // set true and switch off in case a single event in a given LS has all subsys off.
+  physDecl_=true; // set true and switch off in case a single event in a given LS does not have it set.
+  
 }
 
 DQMProvInfo::~DQMProvInfo(){
 }
 
-void DQMProvInfo::beginRun(const edm::Run& r, const edm::EventSetup &c ) {
+void 
+DQMProvInfo::beginRun(const edm::Run& r, const edm::EventSetup &c ) {
 
   makeProvInfo();
-      
+  
+  dbe_->setCurrentFolder(subsystemname_ +"/EventInfo/");
+
+  if (makedcsinfo_)
+  {
+    reportSummaryMap_ = dbe_->book2D("reportSummaryMap",
+                	"Info Report Summary Map", 15, 1., 16., 10, 1., 11.);
+    reportSummaryMap_->setBinLabel(10, "Physics", 2);
+    reportSummaryMap_->setBinLabel( 9, "CSC",     2);
+    reportSummaryMap_->setBinLabel( 8, "DT",      2);
+    reportSummaryMap_->setBinLabel( 7, "EB",      2);
+    reportSummaryMap_->setBinLabel( 6, "EE",      2);
+    reportSummaryMap_->setBinLabel( 5, "ES",      2);
+    reportSummaryMap_->setBinLabel( 4, "Hcal",    2);
+    reportSummaryMap_->setBinLabel( 3, "Pixel",   2);
+    reportSummaryMap_->setBinLabel( 2, "RPC",     2);
+    reportSummaryMap_->setBinLabel( 1, "Strip",   2);
+    for (int i=1;i<16;i++)
+      for (int j=1;j<11;j++)
+	reportSummaryMap_->setBinContent(i,j,-1.);
+  }
+  else
+  { 
+    reportSummaryMap_ = dbe_->book2D("reportSummaryMap",
+                	"Info Report Summary Map", 1, 0., 1., 1, 0., 1.);
+    reportSummaryMap_->setBinContent(1,1,1.);
+  }
+  
+  reportSummary_=dbe_->bookFloat("reportSummary");
+  reportSummary_->Fill(1.); // to be refined based on some algorithm
+
+  dcsword_=0xffff;
+  physDecl_=true;
 } 
 
 void DQMProvInfo::analyze(const Event& e, const EventSetup& c){
  
 //  makeProvInfo();
+
+  if (makedcsinfo_) 
+  { 
+     makeDcsInfo(e);
+     makeGtInfo(e);
+  }
   return;
+}
+
+void
+DQMProvInfo::endLuminosityBlock(const edm::LuminosityBlock& l, const edm::EventSetup& c)
+{
+
+ 
+  if (!makedcsinfo_) return;
+
+  // put dcsword into reportSummary 
+  // FIXME consider making renderplugin "dynamic"
+  // first move everything to the left by 1
+  for (int i=2;i<16;i++) 
+  {
+    for (int j=1;j<11;j++) 
+    {
+      float cont = reportSummaryMap_->getBinContent(i,j);
+      reportSummaryMap_->setBinContent(i-1,j,cont);
+    }
+  }
+
+  // fill last bin 15 for detector HV
+  for (int j=0;j<9;j++) 
+  {
+     float cont = 0.;
+     if (dcsword_&(0x1<<j)) cont = 1.;
+     cout << j << " " << (0x1<<j) << " " << cont << endl;
+     reportSummaryMap_->setBinContent(15,9-j,cont);
+  }
+  // reset
+  dcsword_=0xffff;
+
+
+  // fill physics decl. bit in y bin 10.
+  if (physDecl_) 
+    reportSummaryMap_->setBinContent(15,10,1.);
+  else
+    reportSummaryMap_->setBinContent(15,10,0.);
+  // reset   
+  physDecl_=true;  
+
+
+  // set labels 
+  int nlumi = l.id().luminosityBlock();
+  char label[10];
+  sprintf(label, "lumi %d", nlumi);
+  reportSummaryMap_->setBinLabel( 15, label ,     1);
+  if (nlumi>15) 
+  {
+    sprintf(label, "lumi %d", nlumi-15);
+    reportSummaryMap_->setBinLabel( 1,  label ,      1);
+  }
+
+  return;
+  
 }
 
 // run showtag command line
@@ -99,7 +199,7 @@ void
 DQMProvInfo::makeProvInfo()
 {
     dbe_->cd() ;
-    dbe_->setCurrentFolder(currentfolder_);
+    dbe_->setCurrentFolder( subsystemname_ + "/" +  provinfofolder_) ;
 
     // if (dbe_->get("ProvInfo/CMSSW")) return ;
     
@@ -118,4 +218,74 @@ DQMProvInfo::makeProvInfo()
     //fileVersion_->Fill(version_);
     
     return ;
+}
+void 
+DQMProvInfo::makeDcsInfo(const edm::Event& e)
+{
+
+  edm::Handle<DcsStatusCollection> dcsStatus;
+  e.getByLabel("scalersRawToDigi", dcsStatus);
+  for (DcsStatusCollection::const_iterator dcsStatusItr = dcsStatus->begin(); 
+                                         dcsStatusItr != dcsStatus->end(); ++dcsStatusItr) 
+  {
+      // cout << "here " << dcsStatusItr->ready() << endl;
+      if (!dcsStatusItr->ready(DcsStatus::CSCp) && 
+          !dcsStatusItr->ready(DcsStatus::CSCm))   
+     	                              dcsword_ = dcsword_ & 0xfffe;
+
+      if (!dcsStatusItr->ready(DcsStatus::DT0) &&
+          !dcsStatusItr->ready(DcsStatus::DTp) &&
+	  !dcsStatusItr->ready(DcsStatus::DTm))   
+	                              dcsword_ = dcsword_ & 0xfffd; 
+
+      if (!dcsStatusItr->ready(DcsStatus::EBp) &&
+          !dcsStatusItr->ready(DcsStatus::EBm))   
+	                              dcsword_ = dcsword_ & 0xfffb;
+
+      if (!dcsStatusItr->ready(DcsStatus::EEp) &&
+          !dcsStatusItr->ready(DcsStatus::EEm))
+	                              dcsword_ = dcsword_ & 0xfff7;
+
+      if (!dcsStatusItr->ready(DcsStatus::ESp) &&
+          !dcsStatusItr->ready(DcsStatus::ESm)) 
+	                              dcsword_ = dcsword_ & 0xffef;
+
+      if (!dcsStatusItr->ready(DcsStatus::HBHEa) &&
+          !dcsStatusItr->ready(DcsStatus::HBHEb) &&
+	  !dcsStatusItr->ready(DcsStatus::HBHEc) &&
+	  !dcsStatusItr->ready(DcsStatus::HF) &&
+	  !dcsStatusItr->ready(DcsStatus::HO)) 
+	                              dcsword_ = dcsword_ & 0xffdf; 
+
+      if (!dcsStatusItr->ready(DcsStatus::BPIX) &&
+          !dcsStatusItr->ready(DcsStatus::FPIX))  
+	                              dcsword_ = dcsword_ & 0xffbf;
+
+      if (!dcsStatusItr->ready(DcsStatus::RPC))   
+                                      dcsword_ = dcsword_ & 0xff7f;
+
+      if (!dcsStatusItr->ready(DcsStatus::TIBTID) &&
+          !dcsStatusItr->ready(DcsStatus::TOB) && 
+	  !dcsStatusItr->ready(DcsStatus::TECp) &&
+	  !dcsStatusItr->ready(DcsStatus::TECm))  
+	                              dcsword_ = dcsword_ & 0xfeff;
+
+      // cout << hex << dcsword_ << endl;
+  }
+      
+  return ;
+}
+
+void 
+DQMProvInfo::makeGtInfo(const edm::Event& e)
+{
+
+  edm::Handle<L1GlobalTriggerReadoutRecord> gtRecord;
+  e.getByLabel("l1GtUnpack", gtRecord);
+  L1GlobalTriggerReadoutRecord const* gtrr = gtRecord.product();
+
+  L1GtFdlWord fdlWord = gtrr->gtFdlWord();
+     cout << "phys decl. bit=" << fdlWord.physicsDeclared() << endl;
+  if (fdlWord.physicsDeclared() !=1) physDecl_=false;
+  return;
 }
--- src/DQMProvInfo.h
+++ src/DQMProvInfo.h
@@ -4,9 +4,9 @@
 /*
  * \file DQMProvInfo.h
  *
- * $Date: 2008/07/06 17:32:30 $
- * $Revision: 1.11 $
- * \author M. Zanetti - INFN Padova
+ * $Date: 2009/12/11 01:08:36 $
+ * $Revision: 1.3 $
+ * \author A.Meyer - DESY
  *
 */
 
@@ -45,17 +45,25 @@ protected:
   /// Analyze
   void analyze(const edm::Event& e, const edm::EventSetup& c);
   void beginRun(const edm::Run& r, const edm::EventSetup& c) ;
+  void endLuminosityBlock(const edm::LuminosityBlock& l, const edm::EventSetup& c);
 
 private:
 
   std::string getShowTags(void);
   void makeProvInfo();  
+  void makeDcsInfo(const edm::Event& e);  
+  void makeGtInfo(const edm::Event& e);
 
   DQMStore *dbe_;
 
   edm::ParameterSet parameters_;
   
-  std::string currentfolder_;
+  std::string provinfofolder_;
+  std::string subsystemname_;
+  bool makedcsinfo_;
+  
+  unsigned int dcsword_;
+  bool physDecl_;
 
    // histograms
   MonitorElement * versCMSSW_ ;
@@ -69,6 +77,8 @@ private:
   MonitorElement * isComplete_;
   MonitorElement * fileVersion_;
   
+  MonitorElement * reportSummary_;
+  MonitorElement * reportSummaryMap_;
   
 };
 
